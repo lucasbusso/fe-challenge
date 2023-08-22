c76579bca3cdb00590969604b490429e55f3c879
Lucas Busso <lucasxbusso@gmail.com>
2023-08-22 14:35:02 -0300

    emoji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-full grid place-content-center text-2xl ">
-      Esta es la lista de recursos de UIZard
+      👈 Esta es la lista de recursos de UIZard
     </div>
   );
 }
