d66b8ed5d1a9ea1432da347a0d81261b43e7d36a
Lucas Busso <lucasxbusso@gmail.com>
2023-08-21 22:58:20 -0300

    data fetch component

--- src/app/components/ListItems.tsx
+++ src/app/components/ListItems.tsx
@@ -1,13 +1,11 @@
-export default async function IdPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function ListItemsComponent({ id }: { id: number }) {
   const post = await fetch(
     `${process.env.INDIVIDUAL_STORY}${id}.json?print=pretty`
   ).then(
     (res) => res.json() as Promise<{ url: string; title: string; by: string }>
   );
 
-  return <iframe className="w-full h-full" src={post.url} />;
+  return (
+    <li className="truncate leading-[64px] px-4">{post.title || post.by}</li>
+  );
 }
